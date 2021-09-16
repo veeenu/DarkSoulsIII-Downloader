66625ef5418dcd4e8b8f374b6f333d5bd6e85c1c
Andrea Venuta <venutawebdesign@gmail.com>
2021-09-16 12:20:20 +0200

    Modified manifest IDs, added manifest patcher

--- build.py
+++ build.py
@@ -1,21 +1,39 @@
+import io
 import os
 import colorama
 import shutil
+import tqdm
+import requests
 import subprocess
+import zipfile
 
 GREEN = colorama.Fore.GREEN
 BLUE = colorama.Fore.BLUE
 RESET = colorama.Style.RESET_ALL
 
+
 def exec(cmd):
   print(f'\n  {BLUE}> {GREEN}{" ".join(cmd)}{RESET}\n')
   subprocess.run(cmd)
+
+def build_depotdownloader():
+  exec([
+    'docker', 'run', '--rm', '-it',
+    '--volume', f'{os.getcwd()}/DepotDownloader:/artifacts',
+    'mcr.microsoft.com/dotnet/sdk:5.0',
+    'bash', '-c',
+    '''
+        git clone https://github.com/SteamRE/DepotDownloader && 
+        cd DepotDownloader &&
+        dotnet publish -r win-x64 --self-contained true -o /artifacts
+    '''
+  ])
 
 def build():
   colorama.init()
 
   cleanup = True
-  build_depot_downloader = True
+  fetch_tools = True
   build_crate = True
 
   if cleanup:
@@ -25,20 +43,44 @@ def build():
     os.mkdir('build/tmp')
     os.mkdir('build/package')
   
-  if build_depot_downloader:
+  if fetch_tools:
     os.chdir('build/tmp')
-    exec(['git', 'clone', 'https://github.com/veeenu/DepotDownloader'])
-    os.chdir('DepotDownloader')
-    exec(['dotnet', 'restore'])
-    exec(['dotnet', 'publish', '-c', 'Release', '-r', 'win-x64', '--self-contained', 'true'])
-    os.chdir('../..')
-    shutil.copytree('tmp/DepotDownloader/DepotDownloader/bin/x64/Release/netcoreapp2.0/win-x64/publish', 'package/DepotDownloader')
+
+    print(f'\n  {BLUE}* {GREEN}Fetching and building DepotDownloader...{RESET}\n')
+
+    exec([
+      'docker', 'run', '--rm', '-it',
+      '--volume', f'{os.getcwd()}/DepotDownloader:/artifacts',
+      'mcr.microsoft.com/dotnet/sdk:5.0',
+      'bash', '-c',
+      'git clone https://github.com/SteamRE/DepotDownloader && ' +
+      'cd DepotDownloader && '+
+      'dotnet restore && '+
+      'dotnet publish -r win-x64 -c Release --self-contained true -o /artifacts ' +
+      '/p:PublishTrimmed=true /p:TrimMode=link'
+    ]) 
+
+
+    print(f'\n  {BLUE}* {GREEN}Fetching ManifestPatcher...{RESET}\n')
+    resp = requests.get('https://api.github.com/repos/fifty-six/zig.SteamManifestPatcher/releases').json()
+    asset = resp[0]['assets'][0]
+    filename = asset['name']
+    smp_exe = requests.get(asset['browser_download_url'], stream=True)
+    with open('SteamDepotDownpatcher.exe', 'wb') as fp:
+        for chunk in tqdm.tqdm(smp_exe.iter_content(chunk_size=128*1024)):
+            if chunk:
+                fp.write(chunk)
+
+    os.chdir('..')
+    shutil.copytree('tmp/DepotDownloader', 'package/darksouls3-downloader-tools')
+    shutil.copy('tmp/SteamDepotDownpatcher.exe', 'package/darksouls3-downloader-tools')
     os.chdir('..')
 
   if build_crate:
     exec(['cargo', 'build', '--release'])
     shutil.copy('target/release/darksouls3-downloader.exe', 'build/package')
 
+  shutil.make_archive('build/darksouls3-downloader', 'zip', 'build/package')
 
 if __name__ == '__main__':
   build()
--- src/main.rs
+++ src/main.rs
@@ -1,114 +1,197 @@
 use console::Term;
-use dialoguer::{Select, Input, PasswordInput};
+use dialoguer::{Input, PasswordInput, Select};
 use std::path::PathBuf;
 use std::process::{Command, Stdio};
 
-const DEPOT_DOWNLOADER_PATH: &'static str = "DepotDownloader/DepotDownloader.exe";
+const DEPOT_DOWNLOADER_PATH: &'static str = "darksouls3-downloader-tools/DepotDownloader.exe";
+const MANIFEST_PATCHER_PATH: &'static str = "darksouls3-downloader-tools/SteamDepotDownpatcher.exe";
 
 #[derive(Debug)]
 struct Manifest {
-  app_id: u64,
-  depot: u64,
-  manifest: u64
+    app_id: u64,
+    depot: u64,
+    manifest: u64,
 }
 
 #[derive(Debug)]
 struct PatchSet {
-  patch_id: String,
-  target_folder: String,
-  manifests: Vec<Manifest>
+    patch_id: String,
+    target_folder: String,
+    manifests: Vec<Manifest>,
+}
+
+fn patch_steam() -> Result<(), String> {
+    let mut exe_path = std::env::current_exe()
+        .map_err(|e| format!("{}", e))?
+        .parent()
+        .ok_or_else(|| String::from("Directory has no parent"))?
+        .to_path_buf();
+    exe_path.push(PathBuf::from(MANIFEST_PATCHER_PATH));
+    Command::new(&exe_path)
+        .stdin(Stdio::inherit())
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .spawn()
+        .map_err(|e| format!("Couldn't start subprocess: {}", e))?
+        .wait()
+        .map_err(|e| format!("Couldn't download depot: {}", e))?;
+
+    Ok(())
 }
 
 impl Manifest {
-  fn new(app_id: u64, depot: u64, manifest: u64) -> Manifest {
-    Manifest { app_id, depot, manifest }
-  }
+    fn new(app_id: u64, depot: u64, manifest: u64) -> Manifest {
+        Manifest {
+            app_id,
+            depot,
+            manifest,
+        }
+    }
 
-  fn download(&self, path: &str, username: &str, password: &str) {
-    println!("Downloading {} | {} | {} to {}", self.app_id, self.depot, self.manifest, path);
-    let mut exe_path = std::env::current_exe()
-      .unwrap()
-      .parent()
-      .unwrap()
-      .to_path_buf();
-    exe_path.push(PathBuf::from(DEPOT_DOWNLOADER_PATH));
-    Command::new(&exe_path)
-      .stdin(Stdio::inherit())
-      .stdout(Stdio::inherit())
-      .stderr(Stdio::inherit())
-      .args(&[
-        "-app", &format!("{}", self.app_id),
-        "-depot", &format!("{}", self.depot),
-        "-manifest", &format!("{}", self.manifest),
-        "-username", username,
-        "-password", password,
-        "-dir", path
-      ])
-      .spawn()
-      .expect("Couldn't start subprocess")
-      .wait()
-      .expect("Couldn't download depot");
-  }
+    fn download(&self, path: &str, username: &str, password: &str) -> Result<(), String> {
+        println!(
+            "Downloading {} | {} | {} to {}",
+            self.app_id, self.depot, self.manifest, path
+        );
+        let mut exe_path = std::env::current_exe()
+            .map_err(|e| format!("{}", e))?
+            .parent()
+            .ok_or_else(|| String::from("Directory has no parent"))?
+            .to_path_buf();
+        exe_path.push(PathBuf::from(DEPOT_DOWNLOADER_PATH));
+        Command::new(&exe_path)
+            .stdin(Stdio::inherit())
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .args(&[
+                "-app",
+                &format!("{}", self.app_id),
+                "-depot",
+                &format!("{}", self.depot),
+                "-manifest",
+                &format!("{}", self.manifest),
+                "-username",
+                username,
+                "-password",
+                password,
+                "-dir",
+                path,
+            ])
+            .spawn()
+            .map_err(|e| format!("Couldn't start subprocess: {}", e))?
+            .wait()
+            .map_err(|e| format!("Couldn't download depot: {}", e))?;
+
+        Ok(())
+    }
 }
 
 impl PatchSet {
-  fn new(desc: &str, target_folder: &str, manifests: Vec<Manifest>) -> PatchSet {
-    PatchSet {
-      patch_id: desc.to_owned(),
-      target_folder: target_folder.to_owned(),
-      manifests
+    fn new(desc: &str, target_folder: &str, manifests: Vec<Manifest>) -> PatchSet {
+        PatchSet {
+            patch_id: desc.to_owned(),
+            target_folder: target_folder.to_owned(),
+            manifests,
+        }
     }
-  }
 
-  fn download(&self, username: &str, password: &str) {
-    println!("Downloading patch {}", self.patch_id);
-    for manifest in &self.manifests {
-      manifest.download(&self.target_folder, username, password);
+    fn download(&self, username: &str, password: &str) -> Result<(), String> {
+        println!("Downloading patch {}", self.patch_id);
+        for manifest in &self.manifests {
+            manifest.download(&self.target_folder, username, password)?;
+        }
+        Ok(())
     }
-  }
 }
 
 impl ToString for PatchSet {
-  fn to_string(&self) -> String {
-    self.patch_id.clone()
-  }
+    fn to_string(&self) -> String {
+        self.patch_id.clone()
+    }
+}
+
+fn get_credentials(term: &Term, patches: &[PatchSet]) -> Result<(String, String, usize), String> {
+    let username = Input::<String>::new()
+        .with_prompt("Username")
+        .interact()
+        .map_err(|e| format!("{}", e))?;
+
+    let password = PasswordInput::new()
+        .with_prompt("Password")
+        .interact()
+        .map_err(|e| format!("{}", e))?;
+
+    let chosen = Select::new()
+        .items(&patches)
+        .default(0)
+        .with_prompt("Patch to download")
+        .interact_on(&term)
+        .map_err(|e| format!("{}", e))?;
+
+    Ok((username, password, chosen))
 }
 
 fn main() {
-  let patches = vec![
-    PatchSet::new("Ver 1.04 / Reg. 1.05 (Any%)", "v1.04", vec![
-      Manifest::new(374320, 374321, 7552375533020122115)
-    ]),
-    PatchSet::new("Ver 1.08 / Reg. 1.22 (Any% No TearDrop)", "v1.08", vec![
-      Manifest::new(374320, 374321, 8582612580945347462),
-      Manifest::new(374320, 506970, 6554758566340383649),
-    ]),
-    PatchSet::new("Ver 1.12 / Reg. 1.32 (All Bosses)", "v1.12", vec![
-      Manifest::new(374320, 374321, 627215463385297895),
-      Manifest::new(374320, 506970, 2441039608905796121),
-      Manifest::new(374320, 506971, 2910451490878009764),
-    ]),
-  ];
+    let patches = vec![
+        PatchSet::new(
+            "Ver 1.04 / Reg. 1.05 (Any%)",
+            "v1.04",
+            vec![Manifest::new(374320, 374321, 7552375533020122115)],
+        ),
+        PatchSet::new(
+            "Ver 1.08 / Reg. 1.22 (Any% No TearDrop)",
+            "v1.08",
+            vec![
+                Manifest::new(374320, 374321, 8582612580945347462),
+                Manifest::new(374320, 506970, 6721400008311921502),
+            ],
+        ),
+        PatchSet::new(
+            "Ver 1.12 / Reg. 1.32 (All Bosses)",
+            "v1.12",
+            vec![
+                Manifest::new(374320, 374321, 627215463385297895),
+                Manifest::new(374320, 506970, 1039201774019499553),
+                Manifest::new(374320, 506971, 8981258257885112790),
+            ],
+        ),
+    ];
 
-  let term = Term::stdout();
-  term.clear_screen().unwrap();
+    let term = Term::stdout();
+    term.clear_screen().unwrap();
 
-  let username = Input::<String>::new()
-    .with_prompt("Username")
-    .interact()
-    .unwrap();
+    println!(
+        r#"********************************************************************************
+* Dark Souls III Patch downloader                                              *
+********************************************************************************
 
-  let password = PasswordInput::new()
-    .with_prompt("Password")
-    .interact()
-    .unwrap();
+Welcome! To download earlier patches of Dark Souls III from Steam, you will have
+to supply your username and password.
 
-  let chosen = Select::new()
-    .items(&patches)
-    .default(0)
-    .with_prompt("Patch to download")
-    .interact_on(&term)
-    .unwrap();
+You will then be asked to choose which patch you want to download.
 
-  patches[chosen].download(&username, &password);
+If you have it enabled, the Depot Downloader will ask for your 2FA code.
+
+Let's begin!
+
+Patching Steam, enabling manifest download...
+"#
+    );
+
+    if let Err(e) = patch_steam() {
+        println!("\n  ERROR: {}", e);
+        term.read_key().ok();
+        return;
+    }
+
+    println!("\nSteam patched for manifest downloads. Please enter your credentials now!\n");
+
+    if let Err(e) = get_credentials(&term, &patches)
+        .map(|(username, password, chosen)| patches[chosen].download(&username, &password))
+    {
+        println!("\n  ERROR: {}", e);
+    } else {
+        println!("\n ALL DONE!");
+    }
+    term.read_key().ok();
 }
